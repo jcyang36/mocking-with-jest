3babd4e3b0a111a7317683a4edf598fc31f586ca
Joyce Yang <jcyang@hmc.edu>
2017-11-21 11:33:17 -0500

    mock changes

---
+++ src/_tests_/github.test.js
@@ -0,0 +1,17 @@
+/* eslint-env jest */
+
+const github=require('../api/github')
+//A simple example test
+
+describe('#getUser() using Promises', ()=>{
+    it('should load user data', ()=>{
+        return github.getUser('vnglst')
+            .then(data=>{
+                expect(data).toBeDefined()
+                expect(data.entity.name).toEqual('Koen van Gilst')
+            })
+    })
+
+})
+
+jest.mock('../api/request')
